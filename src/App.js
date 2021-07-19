0bd21549ee50ed3e25022052ed9a637d06ea0438
IuliiaKalenyk <yulyaa.kalenyk@gmail.com>
2021-07-19 10:13:32 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import { Container } from './components/Container/Container';
 
 class App extends React.Component {
   state = {
-    contacts: [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },],
-    filter: ''
-  }
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
   isInList = (contact, contacts) =>
     contacts.find(cont =>
       cont.name.toLocaleLowerCase().includes(contact.name.toLocaleLowerCase()),
@@ -30,8 +32,8 @@ class App extends React.Component {
     this.isInList(contact, contacts)
       ? alert(`${contact.name} is already in your list`)
       : this.setState(({ contacts }) => ({
-        contacts: [contact, ...contacts],
-      }));
+          contacts: [contact, ...contacts],
+        }));
   };
 
   deleteContact = contactId => {
@@ -43,6 +45,20 @@ class App extends React.Component {
   changeFilter = e => {
     this.setState({ filter: e.currentTarget.value });
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   render() {
     const normalizeContacts = this.state.filter.toLocaleLowerCase();
     const visibleContacts = this.state.contacts.filter(contact =>
@@ -58,8 +74,10 @@ class App extends React.Component {
 
             <h2>Contacts</h2>
             <Filter value={filter} onChange={this.changeFilter} />
-            <ContactList contacts={visibleContacts}
-              onDeleteContact={this.deleteContact} />
+            <ContactList
+              contacts={visibleContacts}
+              onDeleteContact={this.deleteContact}
+            />
           </Container>
         </Section>
       </div>
